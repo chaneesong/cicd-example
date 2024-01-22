9463d0e68bd5049324f40bc85849d400b1d1d4ca
chaneesong <sch102403@gmail.com>
2024-01-23 06:47:54 +0900

    env test

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,9 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello CICD!!';
+    const consoleTest = process.env.CONSOLE_TEST;
+    const test = process.env.ENV_TEST;
+    console.log(consoleTest);
+    return `${test} CICD!!`;
   }
 }
